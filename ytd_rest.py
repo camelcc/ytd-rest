1d30da8ff8c1f71b6bc9c0f800bdbdb30d0584fd
camel <camel.young@gmail.com>
2018-03-13 23:21:54 -0700

    more data

--- ytd_rest.py
+++ ytd_rest.py
@@ -4,6 +4,8 @@ from urllib.parse import unquote
 
 import youtube_dl
 from flask import Flask, jsonify, request
+from youtube_dl import downloader
+from youtube_dl.downloader import http
 
 app = Flask(__name__)
 
@@ -17,13 +19,28 @@ def parse():
     # realUrl = base64.urlsafe_b64decode(url).decode('utf-8')
     ydl_opts = {
         "nocheckcertificate": True,
-        "format": 'best[ext=mp4]'
+        "format": 'best[ext=mp4]',
+        "simulate": True
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        result = ydl.extract_info(
-            page,
-            download=False  # We just want to extract the info
-        )
+        result = ydl.extract_info(page)
+        if 'entries' in result:
+            for entry in result['entries']:
+                entry['filename'] = ydl.prepare_filename(entry)
+
+                fd = downloader.get_suitable_downloader(entry, ydl_opts)
+                if fd is http.HttpFD:
+                    entry['is_http'] = True
+                else:
+                    entry['is_http'] = False
+        else:
+            result['filename'] = ydl.prepare_filename(result)
+
+            fd = downloader.get_suitable_downloader(result, ydl_opts)
+            if fd is http.HttpFD:
+                result['is_http'] = True
+            else:
+                result['is_http'] = False
 
         info = {}
         if 'entries' in result:
@@ -38,9 +55,12 @@ def parse():
                 videoInfo['title'] = video['title']
                 videoInfo['url'] = video['url']
                 videoInfo['ext'] = video['ext']
+                videoInfo['filename'] = video['filename']
+                videoInfo['is_http'] = video['is_http']
+                videoInfo['http_headers'] = video['http_headers']
 
                 if result.__contains__('is_live'):
-                    videoInfo['isLive'] = video['is_live']
+                    videoInfo['is_live'] = video['is_live']
                 if result.__contains__('description'):
                     videoInfo['description'] = video['description']
                 if result.__contains__('thumbnail'):
@@ -54,9 +74,12 @@ def parse():
             info['title'] = result['title']
             info['url'] = result['url']
             info['ext'] = result['ext']
+            info['filename'] = result['filename']
+            info['is_http'] = result['is_http']
+            info['http_headers'] = result['http_headers']
 
             if result.__contains__('is_live'):
-                info['isLive'] = result['is_live']
+                info['is_live'] = result['is_live']
             if result.__contains__('description'):
                 info['description'] = result['description']
             if result.__contains__('thumbnail'):
