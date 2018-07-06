e3fd52a7fb2756bee14e4ac022200ed1d370de2c
camel <camel.young@gmail.com>
2018-07-05 23:30:48 -0700

    deploy gen files directly

---
+++ dist/precache-manifest.e6b38fe7acc87d3bcf4ddd93b2df7dd9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc19ddd534a4f1ac4386",
+    "url": "/static/js/chunk-vendors.dc19ddd5.js"
+  },
+  {
+    "revision": "87ae21caf8f5bc8b1f8b",
+    "url": "/static/js/app.87ae21ca.js"
+  },
+  {
+    "revision": "87ae21caf8f5bc8b1f8b",
+    "url": "/static/css/app.86045964.css"
+  },
+  {
+    "revision": "5e281d1d7fdb6a9a85cdb057873cd018",
+    "url": "/index.html"
+  }
+];
